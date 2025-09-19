d42add38463912b5921cf2f349e6e01c9bfc2002
RollingCC121 <daniel.gaviria0605@gmail.com>
2025-09-19 11:31:16 -0500

    modificando el docker-compose y Peer/auth para que utilicen variables de entorno

--- Peer/auth.py
+++ Peer/auth.py
@@ -21,3 +21,31 @@ class PeerAuth:
             print("Error conectando a la API:", e)
         
         time.sleep(60)  
+
+''' import os
+    import time
+    import requests
+
+    API_URL = os.environ["API_URL"]
+
+    peer_data = {
+        "username": os.environ["PEER_NAME"],
+        "password": os.environ["PEER_PASSWORD"],
+        "url": os.environ["PEER_URL"],
+    }
+
+    while True:
+        try:
+            response = requests.post(
+                API_URL,
+                params={
+                    "username": peer_data["username"],
+                    "password": peer_data["password"]
+                }
+            )
+            print(f"[{peer_data['username']}] Respuesta del servidor:", response.json())
+        except Exception as e:
+            print(f"[{peer_data['username']}] Error conectando a la API:", e)
+
+        time.sleep(60)
+ '''
